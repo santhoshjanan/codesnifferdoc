8799b50fa50b9b79d8c7f3fb232f110bdd832993
Santhosh J <santhoshj@gmail.com>
2017-03-09 02:18:49 -0500

    Initial Set up for CodeSnifferDoc

---
+++ src/CSDHelper/CodeSniffDoc.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace CSDHelper;
+
+class CodeSniffDoc
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+
+    public static function GetCodeSnifferPage($errorcode = NULL){
+		if(!$errorcode){
+			return "#";
+		}
+
+		preg_match('/[0-9]_[0-9]_[0-9]/', $errorcode, $matches, PREG_OFFSET_CAPTURE);
+		
+		$standard = explode('.', $errorcode)[0];
+
+		$returnpage = "http://squizlabs.github.io/HTML_CodeSniffer/Standards/Section508/";			
+
+		if(strstr($standard, 'Section508')){
+			$returnpage = "http://squizlabs.github.io/HTML_CodeSniffer/Standards/Section508/";
+		}elseif (strstr($standard, 'WCAG2')) {
+			$returnpage = "http://squizlabs.github.io/HTML_CodeSniffer/Standards/WCAG2/".$matches[0][0];
+		}
+
+		return $returnpage;
+	}
+}
---
+++ tests/test.php
@@ -0,0 +1,7 @@
+<?php 
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+
+use CSDHelper\CodeSniffDoc;
+
+echo CodeSniffDoc::GetCodeSnifferPage('WCAG2AA.Principle2.Guideline2_4.2_4_4.H77,H78,H79,H80,H81');
